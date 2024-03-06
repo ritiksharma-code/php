1d38f36ae73b1f6b72d499d3e3e776d1bfbd3b60
Ritik Sharma <ritiksharma0405@gmail.com>
2024-03-06 15:20:19 +0530

    06-03-2024

--- variable.php
+++ variable.php
@@ -19,7 +19,7 @@
         var_dump($d);
         echo "<br>";
         echo "my car is $car <br>";
-        echo "my car is $car ";
+        echo "my car is $car "."<br>";
         echo "my car is ". $car. " color blue";         // Concatenation of string
 
     ?>
