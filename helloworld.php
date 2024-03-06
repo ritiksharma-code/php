123fc305242a002fc3095856a2f7f823e5c605eb
Ritik Sharma <ritiksharma0405@gmail.com>
2024-03-06 15:15:02 +0530

    06-03-2024

---
+++ helloworld.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>First Program</title>
+</head>
+<body>
+    <?php
+        echo "hello world!";
+    ?>
+</body>
+</html>
---
+++ variable.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Variable</title>
+</head>
+<body>
+    <?php
+        $a=10;
+        $b=10.00;
+        $c="Program";
+        $d=True;
+        $car="BMW";
+        var_dump($a);
+        echo "<br>";
+        var_dump($b);
+        echo "<br>";
+        var_dump($c);
+        echo "<br>";
+        var_dump($d);
+        echo "<br>";
+        echo "my car is $car <br>";
+        echo "my car is $car ";
+        echo "my car is ". $car. " color blue";         // Concatenation of string
+
+    ?>
+</body>
+</html>
